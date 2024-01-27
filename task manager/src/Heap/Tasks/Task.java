61caaa56bfdc3dd3469771fde9f1384572cdfc7c
Metiran <iranpourmahdi19@yahoo.com>
2024-01-27 21:03:39 +0330

    Comments added to task class

--- task manager/src/Heap/Tasks/Task.java
+++ task manager/src/Heap/Tasks/Task.java
@@ -7,9 +7,9 @@ public class Task implements Comparable {
     private final String name;
     private final String description;
     private final Date deadline;
+    private Status status;      // can be "to do" or "done"
 
-    private Status status;
-
+    /** require name of task, description, deadline and status*/
     public Task(String name, String description, Date deadline, Status status) {
         this.name = name;
         this.description = description;
@@ -17,26 +17,35 @@ public class Task implements Comparable {
         this.status = status;
     }
 
+    /** get name of task*/
     public String getName() {
         return name;
     }
 
+    /** get description of task*/
     public String getDescription() {
         return description;
     }
 
+    /** get deadline of task*/
     public Date getDeadline() {
         return deadline;
     }
 
+    /** get status of task*/
     public Status getStatus() {
         return status;
     }
 
+    /** sets status of task*/
     public void setStatus(Status status) {
         this.status = status;
     }
 
+    /** compares a task with this task
+     * by their deadlines and returns -1 for lower
+     * , 0 for equal, 1 for greater
+     */
     @Override
     public int compareTo(Object o) {
         Task that = (Task) o;
